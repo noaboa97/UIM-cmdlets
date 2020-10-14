c1e8fe56088532f7f3e754ab9eae42ebb9319f56
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-10-14 16:47:39 +0200

    Init

---
+++ Get-UIMRobots.ps1
@@ -0,0 +1,12 @@
+﻿function Get-UIMRobots{
+    Param (
+            [parameter(valuefrompipeline = $true, mandatory = $true, HelpMessage = "Enter Authorization", Position = 0)]
+            [System.Collections.Hashtable]$Header,
+            [parameter(valuefrompipeline = $true, mandatory = $true, HelpMessage = "Enter Servername", Position = 1)]
+            [string]$Server
+        )
+
+    $request = (Invoke-RestMethod -Headers $Header -Uri "http://$Server/uimapi/robots" -ContentType "application/json").UIMRobots.UIMRobot
+
+    return $request
+}
