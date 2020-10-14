efeaad389dd459e3c9d69f96c53547045d390653
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-10-14 16:45:32 +0200

    Initial

---
+++ Connect-UIMServer.ps1
@@ -0,0 +1,11 @@
+﻿function Connect-UIMServer{
+    $cred = Get-Credential
+
+    $Password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($cred.Password))))
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $cred.UserName,$Password)))
+
+    $Header = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+
+    return $Header
+}
